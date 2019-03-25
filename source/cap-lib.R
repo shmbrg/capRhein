f2c79d2acb6c430cf6883d54503fc5fee1f3de95
shmbrg <sebastian.humberg@gmail.com>
2019-03-25 18:05:55 +0100

    install packages

--- source/cap-lib.R
+++ source/cap-lib.R
@@ -1,12 +1,17 @@
 
 # install libraries automatically, if not installed yet
+relLib <- c("data.table", "lubridate", "zoo", "caret", "h2o", "timetk", "plotly")
+installLib <- relLib[!(relLib %in% installed.packages())]
+if(length(installLib) != 0)
+  l <- lapply(installLib, install.packages)
 
 # load libraries
 library(data.table)           # basic data structure
 library(lubridate)            # for hour, minute
 library(zoo)                  # for na.locf
 library(caret)                # for findCorrelation
-library(h2o)                  # for ML algorithms
+library(h2o)                  # for ML algorithms; package requires installation
+                              # of Java JDK (development kit)
 library(timetk)               # for augmented time series signature
 library(plotly)               # for plots
 
